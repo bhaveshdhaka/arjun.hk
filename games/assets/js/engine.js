b5776afb7dc2e95438975ca9cfd5d59e0efe6e5a
bhaveshdhaka <mail@bhavesh.net>
2026-09-07 21:20:10 +0000

    chore: bump asset version

--- games/assets/js/engine.js
+++ games/assets/js/engine.js
@@ -1,4 +1,4 @@
-import { Store } from './store.js?v=09072111';
+import { Store } from './store.js?v=09072120';
 
 const registry = new Map();
 const ptsFor = (q, ms) => (q && q.type === 'typein' ? 150 : 100) + Math.max(0, 50 - Math.floor(ms / 1000) * 5);
