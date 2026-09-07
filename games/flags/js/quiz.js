1e2760d1ea8c657d1b0e817242eb7ca8c78c403a
bhaveshdhaka <mail@bhavesh.net>
2026-09-07 03:42:33 +0000

    chore: version build.js import too

--- games/flags/js/quiz.js
+++ games/flags/js/quiz.js
@@ -1,5 +1,5 @@
 import { QuizEngine } from '../../assets/js/engine.js?v=20260907b';
-import { buildQuestions, describe, byRegion, WORLD_COUNT, GLOBE } from './build.js';
+import { buildQuestions, describe, byRegion, WORLD_COUNT, GLOBE } from './build.js?v=20260907b';
 import { REGIONS } from './data.js';
 
 QuizEngine.register({
